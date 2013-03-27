a29a4a3e4aad15d015a335d70fe770038e6e53a7
elemonier <emily.lemonier@gmail.com>
2013-03-27 19:09:13 -0400

    new ruby app dawg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hwk1::Application.config.secret_token = '384a3804e8ccfa7113e87ab6bc1ebd194f8ba761f6d3791d658f8f53d256467a805bcd82e2d28fef67a463a04a6958ed3544f02a3630840f0dbb338c3911b840'
